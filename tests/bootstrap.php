2d705ff51ab81ef0c0d082ef85a38041eb196d39
Alvaro Vasconcelos <ialvsconcelos@gmail.com>
2026-03-16 00:00:33 -0300

    tests: stub WP functions in bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,3 +9,19 @@ define( 'ALVSAMTW_ADD_MULTIPLE_TO_WC_CART_URL_PLUGIN_FILE', __DIR__ . '/../trunk
 define( 'ALVSAMTW_ADD_MULTIPLE_TO_WC_CART_URL_PLUGIN_PATH', __DIR__ . '/../trunk/' );
 
 require_once __DIR__ . '/../trunk/includes/class-alvsamtw-add-multiple-to-wc-cart.php';
+
+// Stub WordPress functions used by the plugin.
+if ( ! function_exists( 'sanitize_text_field' ) ) {
+	function sanitize_text_field( $str ) { return strip_tags( $str ); }
+}
+if ( ! function_exists( 'wp_unslash' ) ) {
+	function wp_unslash( $value ) { return stripslashes_deep( $value ); }
+}
+if ( ! function_exists( 'stripslashes_deep' ) ) {
+	function stripslashes_deep( $value ) {
+		return is_array( $value ) ? array_map( 'stripslashes_deep', $value ) : stripslashes( $value );
+	}
+}
+if ( ! function_exists( 'wc_nocache_headers' ) ) {
+	function wc_nocache_headers() {}
+}
